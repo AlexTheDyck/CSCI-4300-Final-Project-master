648cc50fbda4331331fb7806ffbd0b1a4a586c14
AlexTheDyck <alexdyckalex@gmail.com>
2022-05-09 22:07:30 -0400

    logout.php and logout ends session

--- blank.php
+++ blank.php
@@ -375,7 +375,7 @@ if(!isset($_SESSION['userEmail'])){ //if login in session is not set
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="logout.php">Logout</a>
                 </div>
             </div>
         </div>
